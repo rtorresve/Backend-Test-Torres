f590a4f225523838d5211babaa9cc8a5e3bb07a0
Rafael Torres <rafael.torres1@walmart.com>
2021-04-11 14:41:18 -0400

    update settings to use new app.

--- backend_test/urls.py
+++ backend_test/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
 from .utils.healthz import healthz
 
 urlpatterns = [
     path("healthz", healthz, name="healthz"),
+    path(settings.ADMIN_URL, admin.site.urls),
+    path("users/", include("backend_test.users.urls", namespace="users")),
+    path("accounts/", include("allauth.urls")),
 ]
