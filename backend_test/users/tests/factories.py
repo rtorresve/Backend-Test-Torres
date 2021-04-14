628c5743a876947e709293bde2598db2b0afdc2e
Rafael Torres <rafael.torres1@walmart.com>
2021-04-13 22:15:02 -0400

    add users tests

---
+++ backend_test/users/tests/factories.py
@@ -0,0 +1,30 @@
+from typing import Any, Sequence
+
+from django.contrib.auth import get_user_model
+from factory import Faker, post_generation
+from factory.django import DjangoModelFactory
+
+
+class UserFactory(DjangoModelFactory):
+
+    username = Faker("user_name")
+    email = Faker("email")
+
+    @post_generation
+    def password(self, create: bool, extracted: Sequence[Any], **kwargs):
+        password = (
+            extracted
+            if extracted
+            else Faker(
+                "password",
+                length=8,
+                digits=True,
+                upper_case=True,
+                lower_case=True,
+            ).evaluate(None, None, extra={"locale": None})
+        )
+        self.set_password(password)
+
+    class Meta:
+        model = get_user_model()
+        django_get_or_create = ["username"]
--- backend_test/users/urls.py
+++ backend_test/users/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from .views import (
-    user_detail_view, user_redirect_view, user_update_view)
+from .views import DetailUser, EditUser, RedirectUser
 
 app_name = "users"
 urlpatterns = [
-    path("~redirect/", view=user_redirect_view, name="redirect"),
-    path("~update/", view=user_update_view, name="update"),
-    path("<str:username>/", view=user_detail_view, name="detail"),
+    path("~redirect/", RedirectUser.as_view(), name="redirect"),
+    path("~update/", EditUser.as_view(), name="update"),
+    path("<str:username>/", DetailUser.as_view(), name="detail"),
 ]
