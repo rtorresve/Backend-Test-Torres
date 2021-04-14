7d6bf8e61ccd8be7c85a113845cc82700dc98f2b
Rafael Torres <rafael.torres1@walmart.com>
2021-04-13 22:14:22 -0400

    update test settings

--- backend_test/conftest.py
+++ backend_test/conftest.py
@@ -1,8 +1,12 @@
 import os
+import pytest
 
-from django.test import TestCase, TransactionTestCase
+from .users.models import User
+from .users.tests.factories import UserFactory
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend_test.settings")
 
-TestCase.databases = ["default"]
-TransactionTestCase.databases = ["default"]
+
+@pytest.fixture(autouse=True)
+def user() -> User:
+    return UserFactory()
